ce112e63f77c5e7d497a75859d0135ce4e004206
github username <github email address>
2020-04-16 19:14:24 -0400

    cohesion in menus

--- app/resources/menus/ticket_detail_menu.rb
+++ app/resources/menus/ticket_detail_menu.rb
@@ -4,23 +4,23 @@ def ticket_detail_menu(t)
 
   puts "\n\n====================  TICKET DETAIL MENU  ========================\n"
   puts "OPTIONS"
-  puts "  [y] Accept ticket but don't assign" if t.status == 'pending'
-  puts "  [a] Assign ticket to courier" if !t.courier_id
-  puts "  [u] Unassign ticket form #{t.courier.name}" if t.courier_id
+  puts "  [y] ACCEPT ticket but DON'T assign" if t.status == 'pending'
+  puts "  [a] ASSIGN ticket to courier" if !t.courier_id
+  puts "  [u] UNASSIGN ticket from #{t.courier.name}" if t.courier_id
   puts ""
-  puts "  [o] Mark ticket complete" if t.status != 'complete'
-  puts "  [e] Edit ticket details"
-  puts "  [delete] Delete ticket"
+  puts "  [o] Mark ticket COMPLETE" if t.status != 'complete'
+  puts "  [e] EDIT ticket details"
+  puts "  [delete] DELETE ticket"
   puts ""
-  puts "  [tc] Show all incomplete tickets assigned to this courier" if t.courier_id
-  puts "  [ti] Show all incomplete tickets from this client"
+  puts "  [tc] Show all INCOMPLETE tickets assigned to this COURIER" if t.courier_id
+  puts "  [ti] Show all INCOMPLETE tickets from this CLIENT"
   puts ""
-  puts "  [s] Switch to Search Menu"
-  puts "  [c] Switch to Couriers Menu"
-  puts "  [i] Switch to Clients Menu"
-  puts "  [b] or [t] Go Back to Tickets Menu"
-  puts "  [m] Go Back to Main Menu"
-  puts "  [x] Exit Application"
+  puts "  [s] Switch to SEARCH MENU"
+  puts "  [c] Switch to COURIERS MENU"
+  puts "  [i] Switch to CLIENTS MENU"
+  puts "  [t] Go Back to TICKETS MENU"
+  puts "  [m] Go Back to MAIN MENU"
+  puts "  [x] EXIT Application"
 
   print "\n> "
   res = gets.chomp.downcase
@@ -83,7 +83,7 @@ def ticket_detail_menu(t)
     courier_board_menu
   when 'i'
     client_board_menu
-  when 'b' || 't'
+  when 't'
     clear_screen
     ticket_board_menu
   when 'm'
